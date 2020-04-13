230bd4c29148faaeb282fc5755b138e3376355b2
OY Rakuten <longjun.huang@rakuten.com>
2020-04-13 09:45:51 +0800

    JDSV2 update

---
+++ precache-manifest.53705b1c3e037c4342d856dc572665c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bc1184300771420826d6e669599c93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ed0fc4712af4ff44b09",
+    "url": "./static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "310244d543f7d86f38bf",
+    "url": "./static/css/main.72d3f522.chunk.css"
+  },
+  {
+    "revision": "9ed0fc4712af4ff44b09",
+    "url": "./static/js/2.b613375e.chunk.js"
+  },
+  {
+    "revision": "310244d543f7d86f38bf",
+    "url": "./static/js/main.15928cee.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
